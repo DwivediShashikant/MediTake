ee33fbb18c99f9671df41ccbbdb9581ced0a2320
DwivediShashikant <dwivedi.sk@outlook.com>
2017-07-05 19:58:00 +0530

    complete UI

--- app/src/main/java/com/example/shashikant/penorbit/FragmentMedicineToday.java
+++ app/src/main/java/com/example/shashikant/penorbit/FragmentMedicineToday.java
@@ -3,6 +3,7 @@ package com.example.shashikant.penorbit;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ public class FragmentMedicineToday extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater Inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
         View v = Inflater.inflate(R.layout.medicine_today,container,false);
         return v;
     }
--- app/src/main/java/com/example/shashikant/penorbit/FragmentMyProfile.java
+++ app/src/main/java/com/example/shashikant/penorbit/FragmentMyProfile.java
@@ -1,11 +1,14 @@
 package com.example.shashikant.penorbit;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Shashikant on 7/2/2017.
@@ -16,6 +19,15 @@ public class FragmentMyProfile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater Inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = Inflater.inflate(R.layout.my_profile,container,false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        Button button = (Button)v.findViewById(R.id.add_medicine);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(getActivity(),SearchableActivity.class));
+            }
+        });
+
         return v;
     }
 }
